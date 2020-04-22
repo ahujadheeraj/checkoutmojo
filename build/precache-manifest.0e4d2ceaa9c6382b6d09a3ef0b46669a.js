11fd56a58df3ed131a61b60bebd56b18afaee7f6
ahujadheeraj <ahujadheeraj99@gmail.com>
2020-04-22 14:18:22 +0530

    basic homepage setup

---
+++ build/precache-manifest.0e4d2ceaa9c6382b6d09a3ef0b46669a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd77cdf1b933bac3678863466e02e125",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c04fccc90521f1ef288",
+    "url": "/static/css/main.63b7db66.chunk.css"
+  },
+  {
+    "revision": "22546dacbbbf89734535",
+    "url": "/static/js/2.49b67355.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.49b67355.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c04fccc90521f1ef288",
+    "url": "/static/js/main.18f5664e.chunk.js"
+  },
+  {
+    "revision": "b38cb0ff89637096e37f",
+    "url": "/static/js/runtime-main.05e22f88.js"
+  },
+  {
+    "revision": "c9417e9375e250b2ef416cb765a7747d",
+    "url": "/static/media/footer.c9417e93.svg"
+  },
+  {
+    "revision": "e691cd9b2f0d2d17834ee27f581ceb12",
+    "url": "/static/media/install.e691cd9b.svg"
+  }
+]);
